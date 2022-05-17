8907f55289f1cf57a2702e04ba4a36347670ea39
diagodryson <cortezdiago@gmail.com>
2022-05-17 22:18:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const randomNumber = (min,max) => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) +min;
 }
+
+const randomRGB = () => [
+    // entre 0 and 253
+    randomNumber(0, 253),
+    randomNumber(0, 253),
+    randomNumber(0, 253)
+];
